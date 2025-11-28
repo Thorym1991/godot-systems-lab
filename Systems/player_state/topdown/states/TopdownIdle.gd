16884129b28aae1277d309bc2cc0a5d23536c9b3
Thorym1991 <denllity27711@gmx.de>
2025-11-28 08:43:43 +0100

    feat(demo/topdown): added TopdownPlayer demo scene and movement input

--- Systems/player_state/topdown/states/TopdownIdle.gd
+++ Systems/player_state/topdown/states/TopdownIdle.gd
@@ -4,7 +4,10 @@ func id() -> StringName:
 	return &"idle"
 
 func physics_update(delta: float) -> void:
-	var dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	# Spieler wirklich stoppen
+	character.velocity = Vector2.ZERO
 
+	# Wenn Input vorhanden → wechseln zu Move
+	var dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if dir != Vector2.ZERO:
 		machine.change(&"move")
