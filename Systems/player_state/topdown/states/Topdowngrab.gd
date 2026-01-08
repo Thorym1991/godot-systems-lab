6cbe9601887693c8285d2d424441d432b1f4c959
Thorym1991 <denllity27711@gmx.de>
2026-01-08 13:01:04 +0100

    feat(grab): support toggle mode and prevent instant release on enter

--- Systems/player_state/topdown/states/Topdowngrab.gd
+++ Systems/player_state/topdown/states/Topdowngrab.gd
@@ -2,10 +2,15 @@ extends PlayerState
 
 @export var release_on_button_up: bool = true
 
+var just_entered := false
+
+
 func id() -> StringName:
 	return &"grab"
 
 func enter(previous: PlayerState) -> void:
+	just_entered = true
+
 	var p := character as TopdownPlayer2D
 	if p.grabbed_block == null:
 		machine.change(&"idle")
@@ -20,18 +25,31 @@ func exit(next: PlayerState) -> void:
 	p.stop_grab()
 
 func physics_update(delta: float) -> void:
+	if just_entered:
+		just_entered = false
+		return
+
 	var p := character as TopdownPlayer2D
 	var block := p.grabbed_block
 	if block == null:
 		machine.change(&"idle")
 		return
 
-	# Hold-to-grab: loslassen => release
-	if release_on_button_up and not Input.is_action_pressed("interact"):
-		block.release()
-		p.stop_grab()
-		machine.change(&"idle")
-		return
+	# Release-Logik: Hold ODER Toggle
+	if release_on_button_up:
+		# Hold-to-grab: loslassen => release
+		if not Input.is_action_pressed("interact"):
+			block.release()
+			p.stop_grab()
+			machine.change(&"idle")
+			return
+	else:
+		# Toggle-grab: erneut drücken => release
+		if Input.is_action_just_pressed("interact"):
+			block.release()
+			p.stop_grab()
+			machine.change(&"idle")
+			return
 
 	# Input
 	var dir := p.input_dir
